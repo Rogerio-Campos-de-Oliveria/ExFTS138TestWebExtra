b584e14f35f21002645560a8f8b52a9cd3bc3e1e
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2024-01-14 19:22:32 -0300

    Ajuste

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testng:testng:7.7.1")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+    implementation("org.seleniumhq.selenium:selenium-java:4.10.0")
+    implementation("io.github.bonigarcia:webdrivermanager:5.5.3")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useTestNG()
 }
