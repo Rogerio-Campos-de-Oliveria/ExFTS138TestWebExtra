87498eb0e5448895ffb057f3a5d705b2e137cc78
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2024-01-19 16:10:10 -0300

    Ajuste

--- src/test/java/webtest/testgiulianaflores2.java
+++ src/test/java/webtest/testgiulianaflores2.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class testgiulianaflores {
+public class testgiulianaflores2 {
     WebDriver driver;
     JavascriptExecutor js;
 
@@ -50,14 +50,18 @@ public class testgiulianaflores {
         driver.findElement(By.cssSelector(".apply-button")).click();
         driver.findElement(By.cssSelector(".item:nth-child(1) > .product-item img")).click();
         driver.findElement(By.id("ContentSite_lbtBuy")).click();
+        driver.findElement(By.cssSelector("div.box_calendario_popup"));
+        driver.findElement(By.cssSelector("data_dia jCalendarDate"));
+        driver.findElement(By.partialLinkText("2024.01.19")).click();
+        driver.findElement(By.cssSelector("Comercial (08:00 às 19:00)")).click();
 
-        driver.findElement(By.cssSelector("li:nth-child(3) > .jPeriodRadio")).click();
+
+
+        //driver.findElement(By.cssSelector("li:nth-child(7) > .jPeriodRadio")).click();
         driver.findElement(By.id("btConfirmShippingData")).click();
         driver.findElement(By.id("ContentSite_lbtBuy")).click();
-
         driver.findElement(By.cssSelector(".jThisBasket")).click();
-        driver.findElement(By.cssSelector("mesma-data")).click();
-        //driver.findElement(By.id("ContentSite_Basketcontrol1_rptBasket_ibtBuy_0")).click();
+        driver.findElement(By.id("ContentSite_Basketcontrol1_rptBasket_ibtBuy_0")).click();
         {
             WebElement element = driver.findElement(By.cssSelector(".owl-item:nth-child(4) > li > a > img"));
             Actions builder = new Actions(driver);
@@ -70,7 +74,7 @@ public class testgiulianaflores {
             builder.moveToElement(element).perform();
         }
         driver.findElement(By.cssSelector(".item:nth-child(1) > .product-item img")).click();
-
+        js.executeScript("window.scrollTo(0,159)");
         {
             WebElement element = driver.findElement(By.id("ContentSite_lbtBuy"));
             Actions builder = new Actions(driver);
